1e4d4eb822c281835d84b2e69156dc6011dc8207
Hamesh <32076413+SharmaHK@users.noreply.github.com>
2023-10-03 16:26:55 -0700

    Update 2.Add-Two-Numbers.py

--- 2.Add-Two-Numbers.py
+++ 2.Add-Two-Numbers.py
@@ -4,13 +4,31 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def iterateList(l1: Optional[ListNode]):
+        len = 0
+
+        while l1.next != NULL:
+            len +=1
+
+        return len
+
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         mult = 1
+        anslist = Optional[ListNode]
+        anslist = l2
         ans = 0
-        for i in range(2):
+
+        len = 1
+        while l1.next != None:
+            len +=1
+            l1 = l1.next
+        print("length: ",len)
+
+        for i in range(len):
             ans += (l1.val * mult)
-            #l1.val = l1.next
+            l1 = l1.next
             mult *= 10
-            print(mult)
-        print(mult)
-        return l1
+            print(ans)
+
+
+        return anslist
