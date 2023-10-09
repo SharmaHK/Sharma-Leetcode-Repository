0eb0690ca6798b18add291bf8973e08d4ba4e293
Hamesh <32076413+SharmaHK@users.noreply.github.com>
2023-10-09 16:40:31 -0700

    Create 20. Valid Parentheses.py

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        #count how many of each are in string and compare? 
+        para_s = para_e = brack_s = brack_e = brace_s = brace_e = False
+
+        while (s):
+            if
+
+        return False
