bad6b4a27311f973ebe165312f27a61490c1d6db
Hamesh <32076413+SharmaHK@users.noreply.github.com>
2023-10-10 19:29:28 -0700

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,3 +1,17 @@
+#https://leetcode.com/problems/valid-parentheses
+class Solution:
+    def isValid(self, s):
+        stack = [] 
+        for i in s: 
+            if i in '([{': 
+                stack.append(i) 
+            else: 
+                if not stack or (i == ')' and stack[-1] != '(') or (i == '}' and stack[-1] != '{') or (i == ']' and stack[-1] != '['):
+                    return False 
+                stack.pop() 
+        return not stack
+#####################################################################
+#First attempt
 class Solution:
     def isValid(self, s: str) -> bool:
         
