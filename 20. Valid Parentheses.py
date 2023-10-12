65a93ab443cf59a579e2af90c2ccc2d3c21f1e1f
Hamesh <32076413+SharmaHK@users.noreply.github.com>
2023-10-11 20:15:19 -0700

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -3,12 +3,12 @@ class Solution:
     def isValid(self, s):
         stack = [] 
         for i in s: 
-            if i in '([{': 
+            if i in '([{': #look for beginning characters
                 stack.append(i) 
-            else: 
+            else: #if a closing character is next and the corresponding opening character isn't on top of the stack, then we return false
                 if not stack or (i == ')' and stack[-1] != '(') or (i == '}' and stack[-1] != '{') or (i == ']' and stack[-1] != '['):
                     return False 
-                stack.pop() 
+                stack.pop() #if current character pairs with character on stack then remove character from stack 
         return not stack
 #####################################################################
 #First attempt
