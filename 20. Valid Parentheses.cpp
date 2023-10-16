6eee83c72b20b965b880570da0207956ba5ffcf8
Hamesh <32076413+SharmaHK@users.noreply.github.com>
2023-10-16 04:12:47 -0700

    Create 20. Valid Parentheses.cpp

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/valid-parentheses
+//This is a one-to-one conversion of my python solution into c++
+//It ran surprisingly fast for such a simple solution. 
+
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> stack;
+        for (char i : s) {
+            if (i == '(' || i == '{' || i == '[') {
+                stack.push(i);
+            } else {
+                if (stack.empty() || (i == ')' && stack.top() != '(') || (i == '}' && stack.top() != '{') || (i == ']' && stack.top() != '[')) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.empty();
+    }
+};
